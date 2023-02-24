7525dd73ac7964a355eaf06c9a02c3484bf589e7
Kdreval <k.dreval@gmail.com>
2023-02-24 09:35:07 -0800

    move out variables to be globally available

--- R/global_vars.R
+++ R/global_vars.R
@@ -1,2 +1,12 @@
 hg19_build_flavours <- c("hg19", "grch37", "hs37d5", "GRCh37")
 hg38_build_flavours <- c("hg38", "grch38", "GRCh38")
+
+# Needed for the feature flattening
+bcl2_feat <- c("BCL2-intronic", "BCL2-TSS", "BCL2")
+myc_feat <- c("MYC-TSS", "MYC")
+bcl6_feat <- c("BCL6_SV", "BCL6")
+pim1_feat <- c("PIM1", "PIM1-TSS")
+btg2_feat <- c("BTG2", "BTG2-intronic")
+
+features <- list(bcl2_feat, myc_feat, bcl6_feat, pim1_feat, btg2_feat)
+names <- c("BCL2_any", "MYC_aSHM", "BCL6_any", "PIM1_any", "BTG2_any")
--- R/helpers.R
+++ R/helpers.R
@@ -1047,19 +1047,10 @@ classify_dlbcl_lymphgenerator <- function(
 
     #####
     # Now flatten the features that we want to be aquished
-    bcl2_feat <- c("BCL2-intronic", "BCL2-TSS", "BCL2")
-    myc_feat <- c("MYC-TSS", "MYC")
-    bcl6_feat <- c("BCL6_SV", "BCL6")
-    pim1_feat <- c("PIM1", "PIM1-TSS")
-    btg2_feat <- c("BTG2", "BTG2-intronic")
-
-    features <- list(bcl2_feat, myc_feat, bcl6_feat, pim1_feat, btg2_feat)
-    names <- c("BCL2_any", "MYC_aSHM", "BCL6_any", "PIM1_any", "BTG2_any")
-
-    for(i in 1:length(names)){
+    for(i in 1:length(GAMBLR.predict:::names)){
         matrix$full <- flatten_feature(
-            names[i],
-            features[[i]],
+            GAMBLR.predict:::names[i],
+            GAMBLR.predict:::features[[i]],
             matrix$full
         )
     }
